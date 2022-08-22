dbec3025da98de41189edcc428789ed8e191be44
Abdulahadniaz <abdulahad1028@gmail.com>
2022-08-22 12:43:15 +0500

    feat: add data for project cards

--- components/home/projectCard.tsx
+++ components/home/projectCard.tsx
@@ -10,9 +10,10 @@ import {
   Link as ChakraLink,
 } from "@chakra-ui/react";
 import Link from "next/link";
-import { PropsWithChildren, useState } from "react";
 import { FiExternalLink } from "react-icons/fi";
 import { VscGithub } from "react-icons/Vsc";
+import { RiBaseStationLine } from "react-icons/Ri";
+import { SiUpwork } from "react-icons/Si";
 
 const ProjectCard = () => {
   return (
@@ -31,7 +32,7 @@ const ProjectCard = () => {
       </Heading>
 
       <VStack className="chars_container">
-        {[1, 2, 3, 4].map((i, key) => {
+        {projects.map((project, key) => {
           return (
             <div key={key}>
               <Box
@@ -43,7 +44,7 @@ const ProjectCard = () => {
                   lg: "650px",
                 }}
                 bg="#202022"
-                py="40px"
+                py="35px"
                 px="30px"
                 _hover={{
                   transform: "scale(1.1)",
@@ -53,22 +54,19 @@ const ProjectCard = () => {
               >
                 <VStack alignItems="left" gap={1}>
                   <Heading size="xs" letterSpacing="3px">
-                    TYPESCRIPT
+                    {project.tech}
                   </Heading>
-                  <Heading size="sm">Dinerojs/Diner.js</Heading>
-                  <Text size="xs">
-                    Create, calculate, and format money in JavaScript and
-                    TypeScript
-                  </Text>
+                  <Heading size="sm">{project.title}</Heading>
+                  <Text size="xs">{project.desc}</Text>
 
-                  <Link href="https://github.com/Abdulahadniaz" passHref>
+                  <Link href={project.extLink} passHref>
                     <ChakraLink target="_blank" _hover={{ textDecor: "none" }}>
                       <HStack gap={1} cursor="pointer">
-                        <Icon as={VscGithub} color="portwhite.500" />
+                        <Icon as={project.icon} color="portwhite.500" />
                         <Text size="xs" color="portwhite.500">
-                          Github Link
+                          {project.iconText}
                         </Text>
-                        <Icon as={FiExternalLink} color="portwhite.500" />
+                        <Icon as={project.linkIcon} color="portwhite.500" />
                       </HStack>
                     </ChakraLink>
                   </Link>
@@ -83,3 +81,51 @@ const ProjectCard = () => {
 };
 
 export default ProjectCard;
+
+const projects = [
+  {
+    tech: "TYPESCRIPT, NEXTJS, CHAKRA UI, ZUSTAND",
+    title: "Intelliahub",
+    desc: "Build a marketplace for financial analysts website from scratch supervised by Auxcube",
+    icon: RiBaseStationLine,
+    iconText: "Live Demo",
+    linkIcon: FiExternalLink,
+    extLink: "",
+  },
+  {
+    tech: "TYPESCRIPT, NEXTJS, CHAKRA UI",
+    title: "Auxcube",
+    desc: "Convert WP-based oragization's website to NextJs, using Typescript and Chakra UI.",
+    icon: RiBaseStationLine,
+    iconText: "Live Demo",
+    linkIcon: FiExternalLink,
+    extLink: "",
+  },
+  {
+    tech: "WORDPRESS, GUTENBERG",
+    title: "Boards On Top",
+    desc: "Developed a personal amazon affiliate website using Gutenberg, a wordpress extension",
+    icon: RiBaseStationLine,
+    iconText: "Live Demo",
+    linkIcon: FiExternalLink,
+    extLink: "https://boardsontop.com/",
+  },
+  {
+    tech: "TYPESCIPT, REACTJS, REDUX TOOLKIT, FAKERJS",
+    title: "Librarium",
+    desc: "A fairly small front-end web-application which serves as book management store",
+    icon: RiBaseStationLine,
+    iconText: "Live Demo",
+    linkIcon: FiExternalLink,
+    extLink: "",
+  },
+  {
+    tech: "",
+    title: "Librarium",
+    desc: "A fairly small front-end web-application which serves as book management store",
+    icon: RiBaseStationLine,
+    iconText: "Live Demo",
+    linkIcon: FiExternalLink,
+    extLink: "",
+  },
+];
